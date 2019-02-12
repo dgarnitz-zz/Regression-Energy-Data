ff316d46f43a6ab687c227e9998401394610e090
dag8 <dag8@pc2-110-l.cs.st-andrews.ac.uk>
2019-02-12 17:12:18 +0000

    Tried to slice data, encountered error wit hNumpy loadtxt(), decided to use Pandas dataframe instead

--- PracticalOneScript.py
+++ PracticalOneScript.py
@@ -4,10 +4,11 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-data = np.loadtxt('ENB2012_data.csv')
+data = np.loadtxt('ENB2012_data.csv') #this does not work, need to use Pandas
 
-x = data[:, 1] #this DOES NOT WORK, need to review data to see how to slice
-y = data[:, 2] #this DOES NOT WORK, need to review data to see how to slice
+x = data[1:, 0:8]   #this DOES NOT WORK, need to review data to see how to slice
+print(x)
+y = data[1:, 8:10]  #this DOES NOT WORK, need to review data to see how to slice
 
 linreg = LinearRegression()
 linreg.fit(x,y)
