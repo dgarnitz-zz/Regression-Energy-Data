c0e15e5cfac0d6bfb009e733b08dec261e870206
dag8 <dag8@pc3-041-l.cs.st-andrews.ac.uk>
2019-02-25 16:33:13 +0000

    Normalized the data using min-max scaler and fit transformation. No longer need to slice out the first row, as the DataFrame does this automatically. Still unclear whether or not the data frame adds in other column and row labels

--- src/PracticalOneScript.py
+++ src/PracticalOneScript.py
@@ -1,18 +1,28 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
 from sklearn.metrics import mean_squared_error
 
-data = pd.read_csv('ENB2012_data.csv')
+data = pd.read_csv('./P1/PracticalOne/src/ENB2012_data.csv')
 
-x = data.iloc[1:, 0:8]
-y = data.iloc[1:, 8:10] 
+data_values = data.values
+min_max_scaler = preprocessing.MinMaxScaler()
+scaled_data = min_max_scaler.fit_transform(data_values)
+normalized_data = pd.DataFrame(scaled_data)
+
+#print(normalized_data)
+
+x = normalized_data.iloc[0:, 0:8]
+y = normalized_data.iloc[0:, 8:10] 
 
 #Data loading was successful, as was data extraction
 #you have two different Y values right now, so this was of doing regression probably wont work
 
 #also the current MSE is extremely high, 9.322784
 #This might be because the data is not currently normalized
+#print(x)
+print(y)
 
 linreg = LinearRegression()
 linreg.fit(x,y)
